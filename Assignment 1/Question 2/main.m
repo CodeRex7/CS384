318cdcc747affbcb5bdee2ab47c35a9e8b64fc6e
AB1908 <abhishekbhattacherjee@live.com>
2018-01-16 14:53:52 +0530

    Add files via upload
    main.m was modified to beautify the graph output. Additionally, insertion sort was implemented as instructed.

--- Assignment 1/Question 2/main.m
+++ Assignment 1/Question 2/main.m
@@ -2,18 +2,25 @@ modbub=zeros(1,10);
 sel=zeros(1,10);
 bub=zeros(1,10);
 num=zeros(1,10);
-for i= 1:10
-  a=floor(10*rand(1,10*i));
-  num(i)=i*10;
-  bub(i)=BubbleSort(a);
-  sel(i)=SelectionSort(a);
-  modbub(i)=modBubbleSort(a);
+ins=zeros(1,10);
+for j= 1:10
+  for i= 1:10
+    a=floor(10*rand(1,10*i));
+    num(i)=i*10;
+    bub(i) = bub(i) + BubbleSort(a) / 10;
+    sel(i) = sel(i) + SelectionSort(a) / 10;
+    modbub(i) = modbub(i) + modBubbleSort(a) / 10;
+    ins(i) =ins(i) + insertionSort(a) / 10;
   end
+ end
+  
 hold on;
-plot(num,bub);
-plot(num,modbub);
-plot(num,sel);
-legend('bubble','modified bubble','selection');
-xlabel("no. of elements");
-ylabel("no. of iterartions");
+title("Iterations vs Data Size of Sorting \nAlgorithms");
+plot(num,bub,"linewidth",2);
+plot(num,modbub,"linewidth",2);
+%plot(num,sel,"linewidth",2);
+plot(num,ins,"linewidth",2);
+legend('Bubble','Modified Bubble','Insertion');
+xlabel("Number of Elements");
+ylabel("Number of Iterations");
 hold off;
