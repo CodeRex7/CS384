c5f993378840d5c232da53119ab0cea913d8be4b
AB1908 <abhishekbhattacherjee@live.com>
2018-01-16 13:47:42 +0530

    Add files via upload

---
+++ CS384-master/README.md
@@ -0,0 +1,4 @@
+# CS384
+Algorithm Analysis and Design Lab
+
+This is a repo to include Octave/MATLAB code for CS384
---
+++ CS384-master/modBubbleSort.m
@@ -0,0 +1,21 @@
+function [iterations] = modBubbleSort(x)
+    %x = input(prompt);
+    index = length(x);
+    iterations = 0;
+    sorted = true;
+    for i = 1:index
+        sorted = true;
+        for j = i:index-1
+            if x(j) > x(j+1)
+                temp = x(j);
+                x(j) = x(j+1);
+                x(j+1) = temp;
+                sorted = false; 
+            end
+            iterations = iterations + 1;
+        end
+        if sorted
+          return;
+        end
+    end
+end
