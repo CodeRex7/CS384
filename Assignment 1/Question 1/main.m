9983f4adbcbccb4adc22b13d0f2a2bb0bc81ea78
AbhishekB1908 <abhishekbhattacherjee@live.com>
2018-01-20 14:36:39 +0530

    Question 1 completed

--- Assignment 1/Question 1/main.m
+++ Assignment 1/Question 1/main.m
@@ -1,5 +1,20 @@
-i = 1;
-prob=0;
+%Coin Toss Simulation Real-time Plot
+
+
+
+%i = 1;
+headsCount=0;
 tossNumber = 0;
-while prob != 0.5
-  prob = prob + coinToss
+plot(tossNumber,headsCount,"color", "red", "linewidth", 3);
+title("Coin Toss Simulation");
+legend('Probability');
+xlabel("Number of Tosses");
+ylabel("Probability of Heads");
+while tossNumber != 10000
+  hold on;
+  headsCount = headsCount + coinToss;
+  tossNumber = tossNumber + 1;
+  plot(tossNumber,headsCount/tossNumber,"color", "red", "linewidth", 3);
+  drawnow();
+  end
+prob = headsCount / (tossNumber-1);
