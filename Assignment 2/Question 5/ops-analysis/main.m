65e717821e870faa81187bb237c8c9b191c8baf5
AB1908 <abhishekbhattacherjee@live.com>
2018-02-11 13:56:01 +0530

    Multiplication analysis graph generated.

--- Assignment 2/Question 5/ops-analysis/main.m
+++ Assignment 2/Question 5/ops-analysis/main.m
@@ -1,19 +1,19 @@
-num = zeros(1,49);
-iterationsCl = zeros(1,49);
-iterationsSt = zeros(1,49);
+num = zeros(1,7);
+iterationsCl = zeros(1,7);
+iterationsSt = zeros(1,7);
 % for i = 1:15
-for i = 2:50
-	inp1 = randi(i*10,i,i);
-	inp2 = randi(i*10,i,i);
-	num(i-1) = i;
+for i = 2:8
+	inp1 = randi(i*10,2^i,2^i);
+	inp2 = randi(i*10,2^i,2^i);
+	num(i-1) = 2^i;
 	iterationsCl(i-1) = iterationsCl(i-1) + classical(inp1,inp2);
-	if (2^ceil(log2(i)) ~= i)
-		padlen = 2^ceil(log2(i));
-		inp1 = postpad(inp1, padlen,0,1);
-		inp1 = postpad(inp1, padlen,0,2);
-		inp2 = postpad(inp2, padlen,0,1);
-		inp2 = postpad(inp2, padlen,0,2);
-	endif
+	% if (2^ceil(log2(i)) ~= i)
+	% 	padlen = 2^ceil(log2(i));
+	% 	inp1 = postpad(inp1, padlen,0,1);
+	% 	inp1 = postpad(inp1, padlen,0,2);
+	% 	inp2 = postpad(inp2, padlen,0,1);
+	% 	inp2 = postpad(inp2, padlen,0,2);
+	% endif
 	iterationsSt(i-1) = iterationsSt(i-1) + strassen2(inp1,inp2);
 % endfor
 endfor
