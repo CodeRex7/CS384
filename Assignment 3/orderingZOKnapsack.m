71538b294df5e4bcf83ff2d44cb48dc2fa251a10
AB1908 <abhishekbhattacherjee@live.com>
2018-03-13 16:20:56 +0530

    Fix ordering 0-1 Knapsack

--- Assignment 3/orderingZOKnapsack.m
+++ Assignment 3/orderingZOKnapsack.m
@@ -2,17 +2,30 @@
 %A ratio list is calculated and sorted in descending order. Items are then added to the knapsack as feasible
 
 
-function [knapsack] = orderingZOKnapsack(itemList, valueList, weightList, maxKnapsackWeight = 11)
-  knapsack = [];
+function [knapsackValue] = orderingZOKnapsack(itemList, weightList, valueList, maxKnapsackWeight = 11)
+  ratioList = cat(1,itemList,weightList,valueList,valueList ./ weightList);
+  ratioList = transpose(ratioList);
+  ratioList = sortrows(ratioList,[-4])
+  [knapsack,knapsackValue] = fillKnapsack(ratioList,maxKnapsackWeight);
+end
+
+function [knapsack,knapsackValue] = fillKnapsack(ratioList, maxKnapsackWeight)
   currentKnapsackWeight = 0;
-  ratioList = cat(2,itemList,valueList ./ weightList);
-  ratioList = sort(ratioList,[-2]);
-  %execute till knapsack full
-  [knapsack,currentKnapsackWeight] = addToKnapsack(knapsack,valueList,maxKnapsackWeight);
+  knapsackValue = 0;
+  knapsack = [];
+  len = length(ratioList);
+  for counter = 1:len
+    if (isFeasible(currentKnapsackWeight,maxKnapsackWeight,ratioList(counter,2)))
+      knapsack = cat(2,ratioList(counter));
+      knapsackValue += ratioList(counter,3);
+      currentKnapsackWeight += ratioList(counter,2);
+      counter++;
+    endif
+  end
+  return;
 end
 
-function [knapsack,currentKnapsackWeight] = addToKnapsack(knapsack, itemList, maxKnapsackWeight)
-end
-
-function checkFeasible
+function [condition] = isFeasible(currentKnapsackWeight,maxKnapsackWeight,itemWeight)
+  condition = currentKnapsackWeight + itemWeight <= maxKnapsackWeight;
+  return;
 end
---
+++ Assignment 3/readme.md
@@ -0,0 +1,5 @@
+You  should  write  two  different  algorithms  (using  subset  sum
+paradigm    and    ordering    paradigm)    and    compare    their
+performance by varying knapsack capacity from
+1 to maximum
+size (Twice the sum of all weights of individual items)
