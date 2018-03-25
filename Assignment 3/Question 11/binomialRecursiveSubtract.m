ee9e99ab226d585998ea9239b23700d0af13af1d
AB1908 <abhishekbhattacherjee@live.com>
2018-03-26 02:51:09 +0530

    Rename method and implement iteration
    For iteration analysis, a wrapper function was added for iteration analysis. This was achieved using global variable as local variable would create conflict in addition of returned values from recursive calls.

--- Assignment 3/Question 11/binomialRecursiveSubtract.m
+++ Assignment 3/Question 11/binomialRecursiveSubtract.m
@@ -1,6 +1,17 @@
 %Three border cases where n = 0, k = 0 , or n = k
 
+function [passes,value] = binomialRecursiveSubtract(n,k)
+  global passes;
+  passes=0;
+
+  [value] = recursiveSubtract(n,k);
+  return;
+end
+
 function [value] = recursiveSubtract(n,k,value=0)
+  global passes;
+  passes++;
+
   if (n==0) || (k==0) || (n == k)
     value = 1;
   elseif (k < n)
