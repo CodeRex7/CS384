759bd7865332ac59174b3da7b9c644d0a4a010d9
AB1908 <abhishekbhattacherjee@live.com>
2018-03-26 02:42:22 +0530

    Optimize space and time complexity
    Further unnecessary iterations and calculations were removed.

--- Assignment 3/Question 11/bottomUpDynamic.m
+++ Assignment 3/Question 11/bottomUpDynamic.m
@@ -1,19 +1,21 @@
 function [passes] = bottomUpDynamic(n, k)
-  [lookupTable,passes] = tableGen([1],n,k);
-  value = lookupTable(n,k)+lookupTable(n,k+1);
+  n++; k++;
+  [lookupTable,passes] = tableGen(n,k);
+  value = lookupTable(n-1,k-1)+lookupTable(n-1,k);
   return;
 end
 
-function [lookupTable,passes] = tableGen(lookupTable, n , k)
+function [lookupTable,passes] = tableGen(n,k)
   passes = 0;
-  for j = 1:n
-      lookupTable = horzcat(lookupTable,zeros(rows(lookupTable),1));
-      appendRow = [1];
-      for i=2:k+1
-        appendRow = horzcat(appendRow,[lookupTable(j,i-1) + lookupTable(j,i)]);
+  lookupTable = zeros(n-1,k);
+  lookupTable(1,1) = lookupTable(2,2) = lookupTable(2,1) = lookupTable(3,3) = lookupTable(3,1) = 1;
+  lookupTable(3,2) = 2;
+  for j = 3:n-1
+      lookupTable(j,1)=1;
+      for i=2:k
+        lookupTable(j,i) = lookupTable(j-1,i-1) + lookupTable(j-1,i);
         passes++;
       endfor
-      lookupTable = vertcat(lookupTable, appendRow);
   endfor
   return;
 end
