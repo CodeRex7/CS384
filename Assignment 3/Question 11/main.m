625df0fee068a1922a9290467f0bef04718a177f
AB1908 <abhishekbhattacherjee@live.com>
2018-03-26 15:23:40 +0530

    Create main for implementation analysis

---
+++ Assignment 3/Question 11/main.m
@@ -0,0 +1,18 @@
+botup = recur = recsu = zeros(1,50);
+
+for i=1:50
+  botup(i) += bottomUpDynamic(i,floor(i/2));
+  recur(i) += binomialRecursive(i,floor(i/2));
+  recsu(i) += binomialRecursiveSubtract(i,floor(i/2));
+endfor
+
+hold on;
+lw = "linewidth";
+plot(botup,lw,3);
+plot(recur,lw,3);
+plot(recsu,lw,3);
+title("Comparison of Binomial Coefficient Computation \nAlgorithms");
+legend('Dynamic Bottom-Up','Classical Recursive','Recursive Subtract',"location","northwest");
+xlabel("Value of n");
+ylabel("Iterations");
+hold off;
