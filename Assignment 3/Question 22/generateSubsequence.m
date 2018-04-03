1694cd3c0f3313c32c93683605a70fd1aa8d6cf3
AB1908 <abhishekbhattacherjee@live.com>
2018-04-03 16:44:14 +0530

    Implement subsequence generator from table

---
+++ Assignment 3/Question 22/generateSubsequence.m
@@ -0,0 +1,39 @@
+function [subsequence] = generateSubsequence(subsequenceTable,x)
+  subsequence = "";
+  i = rows(subsequenceTable);
+  j = columns(subsequenceTable);
+  while (i>0 && j>0)
+    if (i!=1 && j!=1)
+      if (subsequenceTable(i,j) == subsequenceTable(i,j-1))
+        j--;
+      elseif (subsequenceTable(i,j) == subsequenceTable(i-1,j))
+        i--;
+      else
+        subsequence = [x(i),subsequence];
+        i--;
+        j--;
+      endif
+    elseif (i==1 && j!=1)
+      if subsequenceTable(i,j) == subsequenceTable(i,j-1)
+        j--;
+      else
+        subsequence = [x(i),subsequence];
+        i--;
+        j--;
+      endif
+    elseif (i!=1 && j==1)
+      if subsequenceTable(i,j) == subsequenceTable(i-1,j)
+        j--;
+      else
+        subsequence = [x(i),subsequence];
+        i--;
+        j--;
+      endif
+    else
+      if subsequenceTable(i,j) == 1
+        subsequence = [x(i),subsequence];
+        i--;
+      endif
+    endif
+  endwhile
+end
