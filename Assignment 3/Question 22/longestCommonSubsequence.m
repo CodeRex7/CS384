5bae7b1dd07af60bb41315c4702e63d9ccd9ea3b
AB1908 <abhishekbhattacherjee@live.com>
2018-04-08 18:18:53 +0530

    Fix output format
    Although the two necessary values were returned, output format was
    incorrect.

--- Assignment 3/Question 22/longestCommonSubsequence.m
+++ Assignment 3/Question 22/longestCommonSubsequence.m
@@ -1,3 +1,5 @@
-function [subsequence] = longestCommonSubsequence(x,y)
+function [] = longestCommonSubsequence(x,y)
   subsequence = generateSubsequence(generateSubsequenceTable(x,y),x);
+  subsequenceLength = length(subsequence);
+  printf("%d (%s)\n",subsequenceLength,subsequence);
 end
