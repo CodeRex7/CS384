abfbab38df180e6a67b380c72cab241fcfa54457
AB1908 <abhishekbhattacherjee@live.com>
2018-04-17 14:11:33 +0530

    Fix implementation

--- Assignment 3/Question 12/kruskalMST.m
+++ Assignment 3/Question 12/kruskalMST.m
@@ -1,17 +1,17 @@
 %Use a table of edge weights and then sort the weights. Select the lowest weights while checking vertices already included.
 
-function [MSTEdges] = kruskalMST(vertices, edgeWeights)
-    numEdges = columns(edgeWeights)
-    edgeWeights = edgeWeights';
-    edgeWeights = sortrows(edgeWeights,2);
-    verticesIncluded = [];
+function [MSTEdges] = kruskalMST(edges, edgeWeights)
+    numEdges = columns(edgeWeights);
+    [edgeWeights,sortIdx] = sort(edgeWeights);
+    sortedEdgeNames(sortIdx) = edges(:);
+    verticesIncluded = "";
     MSTEdges = [];
     for i = 1:numEdges
-        if ismember(edgeWeights(i,1),verticesIncluded)
+        if ismember(cell2mat(sortedEdgeNames(i)),verticesIncluded)
             continue;
         else
-            MSTEdges = [MSTEdges " " edgeWeights(i,1)];
-            verticesIncluded = [verticesIncluded edgeWeights(i,1)];
+            MSTEdges = [MSTEdges " " sortedEdgeNames(i)];
+            verticesIncluded = [verticesIncluded cell2mat(sortedEdgeNames(i))];
         endif
     endfor
 end
